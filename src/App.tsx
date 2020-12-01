287586d101a4ac5b1d2d6ae1f54cbe4e34fc7f34
Saad <saad_anwer@live.com>
2020-12-01 16:15:55 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import logo from './logo.svg';
+import './App.css';
+import DropDownCustom from './components/DropDown';
+import React from 'react';
+
+
+//import Dropdown from './DropdownMenu';
+
+function App() {
+  const list = [
+    { id:0, key: 
+      'location',
+      selected: false,
+      label: "Item 1", value: "Item One" ,
+      options: [
+        { id:0,label: "Item 1-1", value: "Item one-One" },
+        { id:1,label: "Item 1-2", value: "Item one-Two" }
+      ] },
+    {
+      id:1,
+      key: 'location',
+      selected: false,
+    label: "Item 2", value:"Item two",
+    
+    options: [
+      {id:0, label: "Item 2-1", value: "Item Two-One" },
+      {id:1, label: "Item 2-2", value: "Item Two-Two" }
+    ] 
+  },
+  {id:2, key: 'location',
+   selected: false, 
+   label: "Item 3", 
+   value: "Item Three",
+   options: [
+    {id:0, label: "Item 3-1", value: "Item three-One" },
+    {id:1, label: "Item 3-2", value: "Item three-Two" }
+  ] },
+  {id:3, key: 'location',
+   selected: false, 
+   label: "Item 4", 
+   value: "Item four",
+   options: [] }
+]
+  // const list=[
+  //   {
+  //       id: 0,
+  //       title: 'New York',
+  //       students:[{name:'Name',department:'bscs'}],
+  //       selected: false,
+  //       key: 'location'
+  //   },
+  //   {
+  //     id: 1,
+  //     title: 'Dublin',
+  //     students:[{name:'Name',department:'bscs'}],
+  //     selected: false,
+  //     key: 'location'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'California',
+  //     students:[{name:'Name',department:'bscs'}],
+  //     selected: false,
+  //     key: 'location'
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Istanbul',
+  //     students:[{name:'Name',department:'bscs'}],
+  //     selected: false,
+  //     key: 'location'
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'Izmir',
+  //     students:[{name:'Name',department:'bscs'}],
+  //     selected: false,
+  //     key: 'location'
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'Oslo',
+  //     selected: false,
+  //     students:[{name:'Name',department:'bscs'},{name:'Name',department:'bscs'},{name:'Name',department:'bscs'}],
+  //     key: 'location'
+  //   }
+  // ]
+
+  // useEffect=(()=>{
+  //   const count = list.filter(function(a) { return a.selected; }).length;
+  //   if(count === 0){
+  //     return {headerTitle: list.title}
+  //   }
+  //   else if(count === 1){
+  //     return {headerTitle: `${count} ${list.titleHelper}`}
+  //   }
+  //   else if(count > 1){
+  //     return {headerTitle: `${count} ${list.titleHelper}s`}
+  //   }
+  // },[list])
+
+  
+  return (
+    <div className="App">
+      <DropDownCustom
+         title="Select"
+         list={list}
+        
+      />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DropDown.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import "../styles/global.sass";
+import { ChevronDown, ChevronUp } from "react-feather";
+
+export default function DropDown(props: { list: any; title: any; }) {
+  const list = props.list;
+  const [location, setLocation] = useState(props.list);
+  const [listOpen, setListOpen] = useState(false);
+  const [nestlistOpen, setnestListOpen] = useState(false);
+  const [nestItem, setNestItem] = useState(React.createRef());
+  const [keyword, setkeyword] = useState("");
+  const [headerTitle, setHeadeTitle] = useState(props.title);
+
+  const toggleItem = (id: string | number , key: any) => {
+    let tempp = list[id];
+    setNestItem(tempp);
+    setnestListOpen((prevState) => !prevState);
+    setkeyword("");
+    let temp = list[id];
+    temp.selected = !temp.selected;
+    list[id] = temp;
+    //  setHeadeTitle(list[id].value)
+    setLocation(temp.selected);
+  };
+  const toggleNestItem = (id: React.Key, opId: React.Key) => {
+    let temp = list[id].options[opId];
+    console.log(temp.value);
+    setHeadeTitle(temp.value);
+    setLocation(list[id].selected);
+  };
+
+  const toggleList = () => {
+    setListOpen((prevState) => !prevState);
+    setkeyword("");
+  };
+  return (
+    <div className="dd-wrapper">
+      <div className="dd-headers" onClick={toggleList}>
+        <div className="dd-header-title">{headerTitle}</div>
+        {listOpen ? <ChevronUp size={24} /> : <ChevronDown size={24} />}
+      </div>
+      {listOpen && (
+        <ul className="dd-list">
+          {list.map((item: { id: string | number ; key: any; value: React.ReactNode; }) => (
+            <li
+              className="dd-list-item"
+              key={item.id}
+              onClick={() => toggleItem(item.id, item.key)}
+            >
+              <div className="dd-header">
+                <div className="dd-header-titles">
+                  {item.value}
+                  <div>
+                    {nestlistOpen ? (
+                      <ChevronUp size={24} />
+                    ) : (
+                      <ChevronDown size={24} />
+                    )}
+                  </div>
+                </div>
+
+                {nestlistOpen && nestItem.id === item.id ? (
+                  <ul style={{ display: "flex", flexDirection: "column" }}>
+                    {nestItem.options.map((ite: { id: string | number; value: React.ReactNode; }) => (
+                      <li
+                        key={ite.id}
+                        className="dd-list-item"
+                        onClick={() => toggleNestItem(item.id, ite.id)}
+                      >
+                        <div className="dd-header-title-list">{ite.value}</div>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <ul></ul>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
